f4d60af7926518ed79ae4583623c86b4283562a1
void main() <100292340+Webwizards60@users.noreply.github.com>
2022-09-16 21:24:22 +0530

    Update appnav.js

--- smiley/assets/navigator/appnav.js
+++ smiley/assets/navigator/appnav.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../screens/Login';
 import Register from '../screens/Register';
 import AppLoader from '../screens/loader';
+import Home from '../screens/Home'
 import { useLogin } from './authpro';
 
 const Stack = createNativeStackNavigator();
@@ -11,17 +12,17 @@ const Stack = createNativeStackNavigator();
 function AppNav() {
     const {loginPending} = useLogin();
 
-    return(
-        <>
-            <NavigationContainer independent={true}>
-                <Stack.Navigator screenOptions={{headerShown:false}}>
-                    <Stack.Screen name="Login" component={Login}/>
-                    <Stack.Screen name="Register" component={Register}/>
-                </Stack.Navigator>
-            </NavigationContainer>
-            { loginPending ? <AppLoader/> : null}
-        </>
-        
+    return (
+      <>
+        <NavigationContainer independent={true}>
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen name="Home" component={Home} />
+          </Stack.Navigator>
+        </NavigationContainer>
+        {loginPending ? <AppLoader /> : null}
+      </>
     );
 }
 
